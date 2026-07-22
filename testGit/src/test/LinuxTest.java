d47709187da423c2dd964af2285d1c65f40e4336
sbp1761 <g4u893hfwe98now3t1@hotmail.com>
2026-07-22 14:17:53 +0900

    Linux おやすみなさい。追加

--- testGit/src/test/LinuxTest.java
+++ testGit/src/test/LinuxTest.java
@@ -4,6 +4,7 @@ public class LinuxTest {
 	public static void main(String[] args) {
 		System.out.println("Hello Linux");
 		System.out.println("Good-Bye Linux");
+		System.out.println("おやすみなさい。");
 	}
 
 }
